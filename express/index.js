98c4c6ba9208482b07cd6b9a65322e8524c3c119
devansh rathod <rathoddevansh53@gmail.com>
2024-09-26 16:40:06 +0530

    html rendr

--- express/index.js
+++ express/index.js
@@ -1,15 +1,25 @@
 const express = require("express")
-
+const path = require("path")
 const app = express();
 
+
+const pathdir = path.join(__dirname,"public")
+app.use(express.static(pathdir)) 
+
+
+
 app.get("/",(req,res)=>{
-   res.send("hy this is my  frist express route")
+   res.sendFile(`${pathdir}/Home.html`)
 })
+app.get("/about",(req,res)=>{
+   res.sendFile(`${pathdir}/inde.html`)
+})
+
 app.get("/about",(req,res)=>{
    res.send("hy this is my  frist express about route")
 })
 app.get("/download",(req,res)=>{
-   res.send("download route")
+   res.download(`${pathdir}/inde.html`)
 })
 
 app.listen(3000,()=>{
