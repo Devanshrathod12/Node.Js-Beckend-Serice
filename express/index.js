c551acec3df428016b1b39d5de2c1e8e0b0d3c78
devansh rathod <rathoddevansh53@gmail.com>
2024-09-26 16:57:15 +0530

    free changes

--- express/index.js
+++ express/index.js
@@ -2,11 +2,8 @@ const express = require("express")
 const path = require("path")
 const app = express();
 
-
 const pathdir = path.join(__dirname,"public")
 app.use(express.static(pathdir)) 
-
-
 
 app.get("/",(req,res)=>{
    res.sendFile(`${pathdir}/Home.html`)
