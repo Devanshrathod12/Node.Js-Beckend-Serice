80eccdf9d2bda9fbf1edc490da53ec2780357d20
devansh rathod <rathoddevansh53@gmail.com>
2024-10-01 18:41:06 +0530

    third big change

--- ProjectWithSager/db.js
+++ ProjectWithSager/db.js
@@ -1,10 +1,15 @@
 const mongoose = require("mongoose")//nodejsprojectSager
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
 mongoose.connect("mongodb://127.0.0.1:27017/nodejsprojectSager").then(()=>{
     console.log("db connected succsesfull");
 }).catch((e)=>{
     console.log(e);
     
 })
+
+// incrept hello ko nknou language me convert krta h loki => 12ergdfgaa
+// decript ye incrept languAGE LIKE => 12ergdfgaa convert => hello 
 
 const schema = new mongoose.Schema({
       name:{
@@ -23,8 +28,49 @@ const schema = new mongoose.Schema({
       password:{
         type:String,
         required:true
-      }
+      },
+      confpassword:{
+        type:String,
+        required:true
+      },
+    tokens:[{
+        token:{
+          type:String,
+          required:true
+        }
+    }]
 })
+
+// pre phle  db conept pre function leta h
+//{ name: string; email: string; phone: number; password: string; 
+//confpassword: string; }>>
+//
+// & > users data jo wo input fill krega
+//
+// jisme users ka data strore hoga
+//
+//mongoose.FlatRecord<{ name: string; email: string; phone: number;
+// password: string; confpassword: string; }> 
+//& { ...; } & { ...; }>):
+
+
+//**   this currunt context ko refer k =rta hai esme cunt contxt schema h */
+schema.pre("save",async function(next){
+    this.password = await bcrypt.hash(this.password,10)
+    next()
+})
+
+schema.methods.genrateToken= async function(){
+  try {
+    const tokenuser = jwt.sign({_id:this._id.toString()},"qoiwnerscmwmlsvdblkhrtijfkbnrhtirhbrmbjkkbmfkghjoibjpeohryjbrwjgbmbkvghbo")
+    this.tokens = this.tokens.concat({token:tokenuser})
+    await this.save();
+    return tokenuser
+    
+  } catch (error) {
+    
+  }
+}
 
 const usermodel = mongoose.model("userdetail",schema)
 module.exports=usermodel
--- ProjectWithSager/router.js
+++ ProjectWithSager/router.js
@@ -1,28 +1,49 @@
 const express = require("express")
 const Routes = express.Router()
 const user = require("./db")
+const bcrypt = require("bcryptjs")
 
 Routes.get("/",(req,res)=>{
     res.render("index")
+})
+Routes.get("/loginin",(req,res)=>{
+    res.render("login")
 })
 
 Routes.post("/register",async(req,res)=>{
     try {
         const data = new user(req.body)
+        if (data.password===data.confpassword) {
+        const emailvalidation = await user.findOne({email:data.email})
+        if (emailvalidation) {
+            res.send("email alrady exist")
+        }
+         // token create
+        const token = await data.genrateToken()
+        console.log(token);
+        res.cookie("jwt",token)
         const savedata = await data.save()
-        res.render("login")
+         res.render("login")
+        }else{
+            res.status(400).send("invalid creidiantial email")
+        }
     } catch (error) {
         res.status(404).send(error)
     }
     
 })
 
-Routes.post("/login",async(req,res)=>{
+Routes.post("/login",async(req,res)=>{  // is match chacck krega compare(passwordusers jo user derha hai input me, database mea vaabal data chackrega databasedata.password) chacck krega
     try {
+        const passwordusers = req.body.password;
         const checkemail = req.body.email;
         const databasedata = await user.findOne({email:checkemail})
-        if (databasedata!=null) {   
-            res.send(databasedata)
+        const ismatch = await bcrypt.compare(passwordusers,databasedata.password)
+        if (ismatch) {   
+            // token create login
+            const token = await databasedata.genrateToken()
+            res.cookie("jwt",token)
+            res.render("contect")
         }else{
             res.status(400).send("invalid creidiantial email")
         }
